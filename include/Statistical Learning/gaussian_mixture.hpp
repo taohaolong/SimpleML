1e05acc2893de5e148ae26039cc28d0af3d9e148
sangyunxin <sangyunxin@gmail.com>
2019-05-11 21:50:19 +0800

    测试gmm，修复bug

--- include/Statistical Learning/gaussian_mixture.hpp
+++ include/Statistical Learning/gaussian_mixture.hpp
@@ -9,20 +9,118 @@ namespace sm {
 		public:
 			nc::NdArray<double> weights_;
 			nc::NdArray<double> means_;
-			std::vector<nc::NdArray<double>> covariances_;
+			nc::DataCube<double> covariances_;
 			nc::NdArray<double> lower_bound_;
 
 			GaussianMixture(int n_components = 1, double tol = 1e-3, int max_iter = 100);
 			~GaussianMixture();
 
+			void fit(const nc::NdArray<double>& X, const nc::NdArray<double>& Y);
 			void fit(const nc::NdArray<double>& X);
-			nc::NdArray<double> predict(const nc::NdArray<double>& X);
+			nc::NdArray<int> predict(const nc::NdArray<double>& X);
 
 		private:
 			int n_components_;
 			double tol_;
 			int max_iter_;
+
+			void init_params(const nc::NdArray<double>& X);
 		};
+
+		GaussianMixture::GaussianMixture(int n_components, double tol, int max_iter):
+			n_components_(n_components),
+			tol_(tol),
+			max_iter_(max_iter)
+		{
+
+		}
+
+		GaussianMixture::~GaussianMixture()
+		{
+
+		}
+
+		void GaussianMixture::init_params(const nc::NdArray<double>& X)
+		{
+			nc::Shape s = X.shape();
+			nc::NdArray<double> X_min = X.min(nc::Axis::ROW);
+			nc::NdArray<double> X_max = X.max(nc::Axis::ROW);
+
+			// 初始化均值和协方差矩阵
+			means_ = nc::NdArray<double>(n_components_, s.cols);
+			for (int col = 0; col < s.cols; ++col)
+			{
+				means_.assignCol(col, nc::Random<double>::randFloat(nc::Shape(n_components_, 1), X_min[col], X_max[col]));
+			}
+	
+			for (int i = 0; i < n_components_; ++i)
+			{
+				covariances_.push_back(nc::identity<double>(s.cols));
+			}
+
+			// 初始化分布权重
+			weights_ = nc::Random<double>::rand(nc::Shape(n_components_, 1));
+			weights_ = weights_ / nc::sum(weights_);
+		}
+
+		void GaussianMixture::fit(const nc::NdArray<double>& X, const nc::NdArray<double>& Y)
+		{
+			fit(X);
+		}
+
+		void GaussianMixture::fit(const nc::NdArray<double>& X)
+		{
+			nc::Shape s = X.shape();
+			init_params(X);
+
+			for (int iter = 0; iter < max_iter_; ++iter)
+			{
+				// E step
+				// 1. 计算概率值
+				nc::NdArray<double> tau(n_components_, s.rows);
+				for (int i = 0; i < n_components_; ++i) 
+				{
+					tau.assignRow(i, multivariate_normal(X, means_(i, means_.cSlice()), covariances_[i]));
+				}
+
+				tau = tau * weights_;
+
+				// 2. 概率值均一化（即公式中的w）
+				tau = tau / tau.sum<double>(nc::Axis::ROW);
+
+				// M step
+				nc::NdArray<double> tau_sum = tau.sum<double>(nc::Axis::COL);
+
+				// 1. 计算更新后的均值
+				nc::NdArray<double> new_means = nc::dot(tau, X) / tau_sum;
+
+				if (abs(nc::max(means_ - new_means)(0, 0)) < tol_)
+					break;
+
+				means_ = nc::dot(tau, X) / tau_sum;
+
+				// 2. 计算更新后的方差
+				for (int i = 0; i < n_components_; ++i) {
+					covariances_[i] = nc::dot( ( X - means_(i, means_.cSlice()) ).transpose() * tau(i, tau.cSlice()), X - means_(i, means_.cSlice()) ) / tau_sum(i, 0);
+				}
+				// 3. 计算更新后的分布权重
+				weights_ = tau_sum / s.rows;
+			}
+		}
+
+		nc::NdArray<int> GaussianMixture::predict(const nc::NdArray<double>& X)
+		{
+			nc::Shape s = X.shape();
+			nc::NdArray<int> Y_predict(s.rows, 1);
+
+			for (int row = 0; row < s.rows; ++row)
+			{
+				nc::NdArray<double> dist = euclidean(X(row, X.cSlice()), means_);
+				Y_predict(row, 0) = nc::argmin(dist, nc::Axis::ROW)(0, 0);
+			}
+
+			return Y_predict;
+		}
 
 	}
 }
--- include/Utils/functions.hpp
+++ include/Utils/functions.hpp
@@ -1,21 +1,26 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 namespace sm {
-	bool double_equal(double a, double b) {
+	bool double_equal(double a, double b) 
+	{
 		if (std::abs(a - b) <= (1e-6))
 			return true;
 		return false;
 	}
 
-	double sigmoid(double z) {
+	double sigmoid(double z) 
+	{
 		return 1 / (1 + std::exp(-1 * z));
 	}
 
-	nc::NdArray<double> sigmoid_vec(const nc::NdArray<double>& Z) {
+	nc::NdArray<double> sigmoid_vec(const nc::NdArray<double>& Z) 
+	{
 		nc::Shape s = Z.shape();
 		nc::NdArray<double> h(s);
-		for (int i = 0; i < s.rows; ++i) {
-			for (int j = 0; j < s.cols; ++j) {
+		for (int i = 0; i < s.rows; ++i) 
+		{
+			for (int j = 0; j < s.cols; ++j) 
+			{
 				h(i, j) = sigmoid(Z(i, j));
 			}
 		}
@@ -23,7 +28,8 @@ namespace sm {
 		return h;
 	}
 
-	nc::NdArray<double> euclidean(const nc::NdArray<double>& node, const nc::NdArray<double>& X) {
+	nc::NdArray<double> euclidean(const nc::NdArray<double>& node, const nc::NdArray<double>& X) 
+	{
 		nc::Shape s = X.shape();
 		nc::NdArray<double> dist = nc::sum((X - node) * (X - node), nc::Axis::COL);
 		dist.reshape(s.rows, 1);
@@ -31,11 +37,14 @@ namespace sm {
 		return dist;
 	}
 
-	int find_most(const std::unordered_map<int, int>& m) {
+	int find_most(const std::unordered_map<int, int>& m) 
+	{
 		int res_key = 0;
 		double res_value = DBL_MIN;
-		for (auto it = m.begin(); it != m.end(); it++) {
-			if (it->second > res_value) {
+		for (auto it = m.begin(); it != m.end(); it++) 
+		{
+			if (it->second > res_value) 
+			{
 				res_key = it->first;
 				res_value = it->second;
 			}
@@ -44,7 +53,8 @@ namespace sm {
 		return res_key;
 	}
 
-	std::unordered_map<int, int> values_count(const nc::NdArray<int>& data) {
+	std::unordered_map<int, int> values_count(const nc::NdArray<int>& data) 
+	{
 		std::unordered_map<int, int> m;
 		for (int i = 0; i < data.size(); ++i)
 			if (m.find(data[i]) == m.end())
@@ -54,17 +64,28 @@ namespace sm {
 		return m;
 	}
 
-	double shannon_ent(const nc::NdArray<int>& data) {
+	double shannon_ent(const nc::NdArray<int>& data) 
+	{
 		nc::Shape s = data.shape();
 		std::unordered_map<int, int> label_count = values_count(data);
 
 		double ent = 0, prob = 0;
-		for (std::unordered_map<int, int>::iterator it = label_count.begin(); it != label_count.end(); ++it) {
+		for (std::unordered_map<int, int>::iterator it = label_count.begin(); it != label_count.end(); ++it) 
+		{
 			prob = (double)it->second / s.rows;
 			ent -= prob * log2(prob);
 		}
 
 		return ent;
+	}
+
+	nc::NdArray<double> multivariate_normal(const nc::NdArray<double>& X, const nc::NdArray<double> mu, const nc::NdArray<double> sigma)
+	{
+		nc::Shape s = X.shape();
+		nc::NdArray<double> num = nc::exp( nc::linalg::multi_dot<double>({ (X - mu), nc::linalg::inv(sigma) , (X - mu).transpose() }) * (-0.5) );
+		double denom = pow((2 * PI), 0.5 * s.cols) * pow(nc::linalg::det(sigma), 0.5);
+		nc::NdArray<double> ret = nc::diagonal(num / denom);
+		return ret;
 	}
 }
 #endif // !FUNCTIONS_HPP
